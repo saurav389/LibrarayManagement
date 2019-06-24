63eb2bd736b972ebbe71becf95e7784431cd8fda
saurav389 <34033906+saurav389@users.noreply.github.com>
2019-06-23 19:31:29 -0700

    Add files via upload

---
+++ StudRegistration.py
@@ -0,0 +1,95 @@
+import os
+import csv
+from colorama import Fore,Back,Style
+# Stud_details = {'Reg_no','Stud_id','Roll_no','Class','name','FName','Gender','DOB','DOA'}
+class StudentRegistration():
+    Fieldname = ['Studid','Regno','Rollno','Class','Name','FName','Gender','DOB','DOA','MNo','Email']
+    Filename = "StudentDetails.csv"
+    def get_length(self):
+        if not os.path.isfile(self.Filename):
+            print(Back.RED + "")
+            print(self.Filename, "Not present")
+            with open(self.Filename, "w",newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=self.Fieldname)
+                writer.writeheader()
+            return 0
+        else:
+            with open(self.Filename, "r") as csvfile:
+                reader = csv.reader(csvfile)
+                reader_list = list(reader)
+                return len(reader_list)
+    def GetDetails(self):
+        message = "Registration Page"
+        print(Back.BLUE + message.center(24))
+        studid = self.get_length()
+        print(Fore.GREEN + "")
+        regno = input("Student Registration no ")
+        rollno = input("Student Roll no ")
+        clss = input("Class ")
+        name = input("Student Name ")
+        fName = input("Student Father Name ")
+        gender = input("Student Gender ")
+        dob = input("Student Date Of Birth ")
+        doa = input("Student Date Of Admission ")
+        mno = input("Student Mobile nO ")
+        email = input("Student Email ")
+        studdata = {
+            "studid":studid,
+            "regno":regno,
+            "rollno":rollno,
+            "class":clss,
+            "name":name,
+            "fname":fName,
+            "gender":gender,
+            "dob":dob,
+            "doa":doa,
+            "mno":mno,
+            "email":email
+        }
+        return studdata
+    def InsertData(self):
+        studata = self.GetDetails()
+        print(Fore.LIGHTMAGENTA_EX + "")
+        if not os.path.isfile(self.Filename):
+            message= "Error {Filename} not present".format(Filename = self.Filename)
+            print(Fore.RED + message.center(30,'_'))
+        if studata['studid']>0:
+            with open(self.Filename,"a",newline='') as csvfile:
+                writer = csv.DictWriter(csvfile,fieldnames=self.Fieldname)
+                writer.writerow({
+                    "Studid": studata['studid'],
+                    "Regno": studata['regno'],
+                    "Rollno": studata['rollno'],
+                    "Class": studata['class'],
+                    "Name": studata['name'],
+                    "FName": studata['fname'],
+                    "Gender": studata['gender'],
+                    "DOB": studata['dob'],
+                    "DOA": studata['doa'],
+                    "MNo": studata['mno'],
+                    "Email": studata['email']
+                })
+        else:
+            with open(self.Filename,"w",newline='') as csvfile:
+                writer = csv.DictWriter(csvfile,fieldnames = self.Fieldname)
+                writer.writeheader()
+                writer.writerow({
+                    "Studid": studata['studid'],
+                    "Regno":studata['regno'],
+                    "Rollno":studata['rollno'],
+                    "Class":studata['class'],
+                    "Name":studata['name'],
+                    "FName":studata['fname'],
+                    "Gender":studata['gender'],
+                    "DOB":studata['dob'],
+                    "DOA":studata['doa'],
+                    "MNo":studata['mno'],
+                    "Email":studata['email']
+                })
+            print("\n\t Data Inserted")
+
+
+#student = StudentRegistration()
+#obj.InsertData()
+
+
---
+++ __main__.py
@@ -0,0 +1,68 @@
+from argparse import ArgumentParser
+import csv
+from StudRegistration import StudentRegistration
+from BooksRegistration import BookRegistration
+from issuebook import search_student
+import os
+student = StudentRegistration()
+book = BookRegistration()
+issue = search_student
+file_item_path = os.path.join(os.path.dirname(__file__),"Bookissued.csv")
+filename= file_item_path
+choise = input(" y = Yes / N = No for MAIN MENU")
+while choise is "y":
+    message = "MAIN MENU"
+    print(message.center(30,'*'))
+    m1 = "1. STUDENT REGISTRATION"
+    m2 = "2. BOOK REGISTRATION"
+    m3 = "3. ISSUE BOOK"
+    m4 = "4. SUBMIT BOOK"
+    m5 = "5. CHECK ISSUED BOOK"
+    m6 = "6. PRINT REGISTERED STUDENT DETAILS"
+    m7 = "7. PRINT REGISTERED BOOK DETAILS"
+    print("\n",m1,"\n",m2,"\n",m3,"\n",m4,"\n",m5,"\n",m6,"\n",m7)
+    option = int(input("Please choose the option"))
+    if(option<8):
+        if option is 1:
+            student.InsertData()
+        elif option is 2:
+            book.InsertData()
+        elif option is 3:
+            #SlNo = issue.get_length("self")
+            issue.issue_book("self")
+        elif option is 4:
+            issue.submit_book("self")
+        elif option is 5:
+            mes="Book id = {Bookid}\nBook Name = {BookName}"
+            with open(filename,"r")as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print(mes.format(Bookid=row['Bookid'],BookName=row['BookName']))
+                    #print(row['Bookid'])
+                    #print(row['BookName'])
+        elif option is 6:
+            issue.get_student_details("self")
+        elif option is 7:
+            issue.get_book_details("self")
+    else:
+        print("invalid input\nPlease input less than 7")
+    choise = input("Do you want to continue y = Yes / N = No")
+
+
+
+
+
+
+
+
+""""a=int(input())
+if a is 1:
+    student = StudentRegistration()
+    student.InsertData()
+else:
+    print("Not imported")
+
+print("hello world")
+
+
+parser = ArgumentParser(prog="Library_Manangement")"""""
